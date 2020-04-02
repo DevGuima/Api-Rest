1fd8a011fa3d012b3eb24acc64b24d027c394fa2
DevGuima <glecioheybel@hotmail.com>
2020-04-02 16:59:13 -0300

    Implementação dos metodos post, put e delete

--- apirest/src/main/java/com/cadastros/apirest/resources/PessoaResource.java
+++ apirest/src/main/java/com/cadastros/apirest/resources/PessoaResource.java
@@ -3,9 +3,11 @@ package com.cadastros.apirest.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,6 +42,19 @@ public class PessoaResource {
 		
 	}
 	
+	@DeleteMapping("/pessoa")
+	public void deletaPessoa(@RequestBody Pessoa pessoa) {
+		 pessoaRepository.delete(pessoa);
+		
+		
+	}
+	
+	@PutMapping("/pessoa")
+	public Pessoa atualizaPessoa(@RequestBody Pessoa pessoa) {
+		return pessoaRepository.save(pessoa);
+		
+		
+	}
 	
 	
 	
