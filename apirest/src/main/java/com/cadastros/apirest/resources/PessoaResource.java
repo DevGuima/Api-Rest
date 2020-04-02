b31814d19e320060b10ab2b22c7e243ce852b9c6
DevGuima <glecioheybel@hotmail.com>
2020-04-02 13:58:44 -0300

    Implementação dos metodos get e post na classe PessoaResource

--- apirest/src/main/java/com/cadastros/apirest/resources/PessoaResource.java
+++ apirest/src/main/java/com/cadastros/apirest/resources/PessoaResource.java
@@ -5,14 +5,18 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.annotation.ApplicationScope;
 
 import com.cadastros.apirest.models.Pessoa;
 import com.cadastros.apirest.repository.PessoaRepository;
 
 @RestController
 @RequestMapping(value="/api")
+
 public class PessoaResource {
 	
 	@Autowired
@@ -28,7 +32,13 @@ public class PessoaResource {
 		return pessoaRepository.findById(id);	
 	}
 	
-
+	
+	@PostMapping("/pessoa")
+	public Pessoa salvaPessoa(@RequestBody Pessoa pessoa) {
+		return pessoaRepository.save(pessoa);
+		
+		
+	}
 	
 	
 	
