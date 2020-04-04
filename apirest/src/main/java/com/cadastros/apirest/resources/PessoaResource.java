77fec8487836da79a102eb83bf27ee240648f9da
DevGuima <glecioheybel@hotmail.com>
2020-04-04 02:36:25 -0300

    Implementação do Swagger na api

--- apirest/src/main/java/com/cadastros/apirest/resources/PessoaResource.java
+++ apirest/src/main/java/com/cadastros/apirest/resources/PessoaResource.java
@@ -3,6 +3,7 @@ package com.cadastros.apirest.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,8 +17,13 @@ import org.springframework.web.context.annotation.ApplicationScope;
 import com.cadastros.apirest.models.Pessoa;
 import com.cadastros.apirest.repository.PessoaRepository;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @RequestMapping(value="/api")
+@Api(value = "API REST Pessoas")
+@CrossOrigin(origins="*")
 
 public class PessoaResource {
 	
@@ -25,17 +31,20 @@ public class PessoaResource {
 	PessoaRepository pessoaRepository;
 	
 	@GetMapping ("/pessoas")
+	@ApiOperation(value = "Retorna uma lista de Pessoas")
 	public List<Pessoa> listPessoas(){
 		return pessoaRepository.findAll();	
 	}
 	
 	@GetMapping ("/pessoa/{id}")
+	@ApiOperation(value = "Retorna uma unica Pessoa")
 	public Pessoa listaUnicaPessoa(@PathVariable (value="id") long id){
 		return pessoaRepository.findById(id);	
 	}
 	
 	
 	@PostMapping("/pessoa")
+	@ApiOperation(value = "Salva uma  Pessoa")
 	public Pessoa salvaPessoa(@RequestBody Pessoa pessoa) {
 		return pessoaRepository.save(pessoa);
 		
@@ -43,6 +52,7 @@ public class PessoaResource {
 	}
 	
 	@DeleteMapping("/pessoa")
+	@ApiOperation(value = "Deleta uma  Pessoa")
 	public void deletaPessoa(@RequestBody Pessoa pessoa) {
 		 pessoaRepository.delete(pessoa);
 		
@@ -50,6 +60,7 @@ public class PessoaResource {
 	}
 	
 	@PutMapping("/pessoa")
+	@ApiOperation(value = "Atualiza uma Pessoa")
 	public Pessoa atualizaPessoa(@RequestBody Pessoa pessoa) {
 		return pessoaRepository.save(pessoa);
 		
